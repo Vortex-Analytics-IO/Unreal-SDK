622e0419788706e5a9f3ac0d28b533a5efe127e9
Gaëtan Dezeiraud <gaetan@dezeiraud.com>
2026-01-19 19:45:16 +0100

    Fix: fix a failed to load error at startup of a project

---
+++ Vortex/Source/Private/Vortex.cpp
@@ -0,0 +1,17 @@
+#include "Vortex.h"
+
+#define LOCTEXT_NAMESPACE "FVortexModule"
+
+void FVortexModule::StartupModule()
+{
+
+}
+
+void FVortexModule::ShutdownModule()
+{
+
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FVortexModule, Vortex)
